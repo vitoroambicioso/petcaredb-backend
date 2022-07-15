a2f52db69059e8d632e1ba795736f48fb242bfc2
joaovitorazerd <isidorojoao21@gmail.com>
2022-07-15 16:47:13 -0300

    app

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,37 +8,54 @@ use App\Models\User;
 
 class UserController extends Controller {
 
-    public function create(Request $request){
+    public function create(Request $request)
+    {
         
         if(!empty($request->all())){
             $user = new User;
             $user->name = $request->username;
             $user->email = $request->email;
             $user->password = bcrypt($request->password);
-    
+
             $user->save();
-            dd('cadastrado com sucesso');
+            $json = json_encode($request->all());
+            return response()->json($json, 200);
+            echo "<pre>";
+            print_r($json);
+            echo "</pre>";
         }else{
             dd('nao foi possivel cadastrar');
         }
 
     }
-    public function createPage(){
+    public function createPage()
+    {
         return view('create-page');
     }
-    public function loginPage() {
+    public function loginPage()
+    {
         return view('login-page');
     }
-    public function loginGoogle() {
+    public function loginGooglePage() 
+    {
         return view('login-google');
     }
     
-    public function login(Request $request) {
+    public function login(Request $request)
+    {
         if(Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
-            dd('logou');
+            $json = json_encode($request->all());
+            echo "<pre>";
+            print_r($json);
+            echo "</pre>";
         }else {
             dd('nao logou');
         }
+    }
+
+    public function updatePage()
+    {
+        return view('update-page');
     }
 
 }
